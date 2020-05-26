638748423816e186d55f706ea63bac41d9cd859f
성기동 <dirrksdl@naver.com>
2020-05-26 09:11:01 +0900

    TabBarLabelComp Component add

--- src/settings/navigation/Screen/PickScreen.js
+++ src/settings/navigation/Screen/PickScreen.js
@@ -28,6 +28,7 @@ import rankTab from '../../../pages/Pick/rankTab';
 
 //* 공통 헤더 및 Sider
 import HeaderScreen from './HeaderScreen';
+import TabBarLabelComp from '../../../components/Label/TabBarLabel';
 
 import {Fonts} from '../../../settings/fonts';
 
@@ -97,31 +98,3 @@ export default function PickScreen({navigation}) {
     </Fragment>
   );
 }
-
-const TabBarLabelComp = ({children, focused}) => {
-  return (
-    <Text
-      style={[focused ? CustomStyles.focusedFont : CustomStyles.defualtFont]}>
-      {children}
-    </Text>
-  );
-};
-
-const CustomStyles = StyleSheet.create({
-  focusedFont: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    lineHeight: 20,
-    letterSpacing: -0.09,
-    fontFamily: Fonts.NotoSans,
-    color: '#042B6C',
-  },
-  defualtFont: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    lineHeight: 20,
-    letterSpacing: -0.09,
-    fontFamily: Fonts.NotoSans,
-    color: '#929394',
-  },
-});
